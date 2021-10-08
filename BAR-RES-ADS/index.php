96edd60094add3d86c536961a539422bfc4b0089
Oliver Visoso <ovisoso@ucol.mx>
2021-10-07 23:49:21 -0500

    Menu Responsive
    Agregue el menu responsive para PC y para movil, falta el JS para el movil

--- BAR-RES-ADS/index.php
+++ BAR-RES-ADS/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
     <link rel="shortcut icon" href="img/icono-empresa.png">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
     <!-- LINKS CSS -->
     <link rel="stylesheet" href="css/index.css">
@@ -13,9 +14,37 @@
 </head>
 <body> 
     <header class="p-3 bg-dark text-white">
+        <span class="nav-bar" id="btnMenu"><i class="fas fa-bars">Menu</i></span>
+        <nav class="main-nav">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <ul class="menu col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0" id="menu">
+                    <li class="menu__item"><a href="index.php" class="menu__link">Inicio</a></li>
+                    <li class="menu__item container-submenu">
+                        <a href="#" class="menu__link submenu-btn">Categorias <i class="fas fa-angle-down"></i></a>
+                        <ul class="submenu">
+                            <li class="menu__item"><a href="" class="menu__link">Restaurantes</a></li>
+                            <li class="menu__item"><a href="" class="menu__link">Bares</a></li>
+                            <li class="menu__item"><a href="" class="menu__link">Puestos de comida</a></li>
+                            <li class="menu__item"><a href="" class="menu__link">Restaurant-bar</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu__item"><a href="planes.html" class="menu__link">Planes</a></li>
+                    <li class="menu__item"><a href="" class="menu__link">Negocios</a></li>
+                </ul>
+                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                    <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+                </form>
+                <div class="text-end">
+                    <button onclick="location.href='checkin.php'" type="button" class="btn btn-outline-light me-2">Registrase</button>
+                    <button onclick="location.href='login.php'" type="button"  class="btn btn-warning">Ingresar</button>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- <header class="p-3 bg-dark text-white">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <!-- Espacio para ingresar categorias de forma desplegable -->
+                
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li><a href="#" class="nav-link px-2 text-secondary">Inicio</a></li>
                     <li><a href="#" class="nav-link px-2 text-white">Restaurantes</a></li>
@@ -33,7 +62,7 @@
                 </div>
             </div>
         </div>
-    </header>
+    </header> -->
     <br>
     <main class="container">
         <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
@@ -127,5 +156,8 @@
         </div>
     </footer>
     </div>
+
+    <!-- SCRIPS JS -->
+    <script src="js/index.js"></script>
 </body>
 </html>
