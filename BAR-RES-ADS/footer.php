ccbd275c368eee70159bf06837a0d9f3d701ed13
Oliver Visoso <ovisoso@ucol.mx>
2021-10-12 17:42:54 -0500

    Header y Footer
    Separe en archivos diferentes estos dos apartados y modifique el index

---
+++ BAR-RES-ADS/footer.php
@@ -0,0 +1,34 @@
+<footer class="p-3 bg-dark text-white">
+        <div class="row">
+        <div class="col-2">
+            <h5>Creadores</h5>
+            <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Torres Ivanna</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Escobar Campos Francisco</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Valdez Josselin</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Visoso Oliver</a></li>
+            </ul>
+        </div>
+
+        <div class="col-2">
+            <h5>Roles</h5>
+            <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+            </ul>
+        </div>
+
+        <div class="col-2">
+            <h5>Institucion</h5>
+            <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+            </ul>
+        </div>
+    </footer>
---
+++ BAR-RES-ADS/header.php
@@ -0,0 +1,28 @@
+<header class="p-3 bg-dark text-white">
+        <span class="nav-bar" id="btnMenu"><i class="fas fa-bars">Menu</i></span>
+        <nav class="main-nav">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <ul class="menu col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0" id="menu">
+                    <li class="menu__item"><a href="index.php" class="menu__link">Inicio</a></li>
+                    <li class="menu__item container-submenu">
+                        <a href="#" class="menu__link submenu-btn">Categorias <i class="fas fa-angle-down"></i></a>
+                        <ul class="submenu">
+                            <li class="menu__item"><a href="" class="menu__link">Restaurantes</a></li>
+                            <li class="menu__item"><a href="" class="menu__link">Bares</a></li>
+                            <li class="menu__item"><a href="" class="menu__link">Puestos de comida</a></li>
+                            <li class="menu__item"><a href="" class="menu__link">Restaurant-bar</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu__item"><a href="planes.html" class="menu__link">Planes</a></li>
+                    <li class="menu__item"><a href="" class="menu__link">Negocios</a></li>
+                </ul>
+                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                    <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+                </form>
+                <div class="text-end">
+                    <button onclick="location.href='checkin.php'" type="button" class="btn btn-outline-light me-2">Registrase</button>
+                    <button onclick="location.href='login.php'" type="button"  class="btn btn-warning">Ingresar</button>
+                </div>
+            </div>
+        </nav>
+    </header>
